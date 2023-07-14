0cfb3c65114e9dec7190943c35d671e675d4ecd9
Keita Tabuchi <tabuchi-keita-wm@ynu.jp>
2023-07-14 16:00:11 +0900

    change ui controller version

--- mmg_module/mmg_module/controller_node.py
+++ mmg_module/mmg_module/controller_node.py
@@ -2,64 +2,109 @@
 # -*- coding: utf-8 -*-
 
 import sys
+
 import rclpy
 from rclpy.node import Node
+
+from PyQt5.QtCore import QThread, pyqtSignal
+from PyQt5.QtWidgets import QApplication, QDialog, QWidget
+
 from shipsim_msgs_module.msg import Control
-from sensor_msgs.msg import Joy
+
+from mmg_module.ShipControllerMMG import Ui_ShipControllerMMG
+
 
 class MmgControllerNode(Node):
     """ControllerNode."""
-    n_p=0.0
-    rudder_angle_degree=0.0
 
     def __init__(self):
         """init."""
         super().__init__("controller", namespace="ship1")
-
-        self.declare_parameter("subscribe_address","/joy")
-        subscribe_address=(self.get_parameter("subscribe_address").get_parameter_value().string_value)
-        self.subscription=self.create_subscription(
-            Joy, subscribe_address, self.listener_callback, 10
-        )
-
-        self.declare_parameter("delta_time",0.1)
         self.declare_parameter("publish_address", "/ship1/control_input")
         publish_address = (
             self.get_parameter("publish_address").get_parameter_value().string_value
         )
         self.publisher = self.create_publisher(Control, publish_address, 10)
-        delta_time=self.get_parameter("delta_time").value
-        self.timer=self.create_timer(delta_time, self.sender_callback)
-
-    def listener_callback(self,data):
-        self.n_p += (data.axes[2]-data.axes[5])*0.5
-        self.rudder_angle_degree += -data.axes[0]*0.3
-
-        if self.n_p<0:
-            self.n_p=0.0
-        elif self.n_p>120:
-            self.n_p=120.0
-        if self.rudder_angle_degree<-45:
-            self.rudder_angle_degree=-45.0
-        elif self.rudder_angle_degree>45:
-            self.rudder_angle_degree=45.0
 
 
-    def sender_callback(self):
-        msg=Control()
-        msg.n_p=self.n_p
-        msg.rudder_angle_degree=self.rudder_angle_degree
-        self.publisher.publish(msg)
-        self.get_logger().info('`Publish: n_p="%s", rudder_angle="%s"'%(self.msg.n_p,self.msg.rudder_angle_degree))
+class ControllerNodeWorker(QThread):
+    """ControllerNodeWorker."""
+
+    signal = pyqtSignal()
+
+    def __init__(self):
+        """init."""
+        super(ControllerNodeWorker, self).__init__()
+        self.control_msg = Control()
+        self.rate = 1.0
+
+    def set_control_msg(self, msg):
+        """set_control_msg."""
+        self.control_msg = msg
+
+    def run(self):
+        """run."""
+        rclpy.init()
+        self.node = MmgControllerNode()
+        self.node.create_rate(self.rate)
+        while rclpy.ok():
+            rclpy.spin_once(self.node)
+
+            self.node.publisher.publish(self.control_msg)
+
+            self.node.get_logger().info(
+                'Publishing: n_p="%s", rudder_angle="%s"'
+                % (self.control_msg.n_p, self.control_msg.rudder_angle_degree)
+            )
+
+            # self.control_msg = MMGControl()
+
+
+class ControllerUi(QDialog):
+    """ControllerUI."""
+
+    worker_thread = ControllerNodeWorker()
+
+    def __init__(self, parent=None):
+        """init."""
+        super(ControllerUi, self).__init__(parent)
+        centralWidget = QWidget(self)
+        self.ui = Ui_ShipControllerMMG()
+        self.ui.setupUi(self)
+        self.sampling_freq = float(self.ui.samplingFreqEdit.text())
+
+    def clicked_start(self):
+        """clicked start button"""
+        control_msg = self.worker_thread.control_msg
+        control_msg.n_p = float(self.ui.propellerSlider.value())
+        self.worker_thread.rate = float(self.ui.samplingFreqEdit.text())
+        self.ui.samplingFreqEdit.setEnabled(False)
+        control_msg.rudder_angle_degree = float(self.ui.rudderDial.value())
+        self.worker_thread.start()
+
+    def change_rudder_angle(self):
+        """change rudder angle dial"""
+        rudder_angle_degree = self.ui.rudderDial.value()
+        control_msg = self.worker_thread.control_msg
+        control_msg.rudder_angle_degree = float(rudder_angle_degree)
+
+    def change_shaft_revolution(self):
+        """change shaft revolution slider"""
+        n_p = self.ui.propellerSlider.value()
+        control_msg = self.worker_thread.control_msg
+        control_msg.n_p = float(n_p)
+
+    def clicked_stop(self):
+        """clicked stop button"""
+        self.worker_thread.control_msg = Control()
 
 
 def main(args=None):
     """Run main."""
-    rclpy.init(args=args)
-    node=MmgControllerNode()
-    rclpy.spin(node)
-    node.destroy_node()
-    rclpy.shutdown()
+    app = QApplication(sys.argv)
+    window = ControllerUi()
+    window.show()
+    sys.exit(app.exec_())
 
 
 if __name__ == "__main__":
