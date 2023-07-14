2da8a8c80b6da6529453e07bed09aa91528c1695
Keita Tabuchi <80773261+Keita058@users.noreply.github.com>
2023-07-14 15:32:45 +0900

    Add simulator ROS2 files

---
+++ actuator_module/actuator_module/actuator_node.py
@@ -0,0 +1,83 @@
+import rclpy
+from rclpy.node import Node
+from shipsim_msgs_module.msg import Control
+
+class ActuatorNode(Node):
+
+    n_p=0.0
+    current_n_p=0.0
+    rudder_angle_degree=0.0
+    current_angle=0.0
+
+    actuated_list={'n_p':[current_n_p],'rudder_angle':[current_angle]}
+
+    def __init__(self):
+        super().__init__("actuator",namespace="ship1")
+
+        self.declare_parameter("subscribe_address","/ship1/control_input")
+        subscribe_address=(self.get_parameter("subscribe_address").get_parameter_value().string_value)
+        self.subscription=self.create_subscription(
+            Control, subscribe_address, self.listener_callback, 10
+        )
+
+        self.declare_parameter("publish_address","/ship1/cmd_input")
+        self.declare_parameter("delta_time",0.01)
+        publish_address=(self.get_parameter("publish_address").get_parameter_value().string_value)
+        self.pub_actuator=self.create_publisher(Control, publish_address, 10)
+        delta_time=self.get_parameter("delta_time").value
+        self.timer=self.create_timer(delta_time, self.sender_callback)
+
+    def listener_callback(self,msg):
+        self.get_logger().info('Subscribe: n_p="%s", rudder_angle="%s"'%(msg.n_p,msg.rudder_angle_degree))
+        self.n_p=msg.n_p
+        self.rudder_angle_degree=msg.rudder_angle_degree
+
+    def sender_callback(self):
+        self.pub_actuator_msg=Control()
+
+        delta_time=self.get_parameter("delta_time").value
+        self.pub_actuator_msg=self.actuated(self.n_p, self.rudder_angle_degree,delta_time)
+
+        self.pub_actuator.publish(self.pub_actuator_msg)
+        self.get_logger().info('Publish: n_p="%s", rudder_angle="%s"'%(self.pub_actuator_msg.n_p, self.pub_actuator_msg.rudder_angle_degree))
+
+    def actuated(self,n_p,rudder_angle,delta_time):
+        actuated_msg=Control()
+        ret_n_p=ret_angle=0.0
+
+        current_n_p=self.actuated_list['n_p'][-1]
+        d_n_p=n_p-current_n_p
+        if abs(d_n_p)<0.5*delta_time:
+            ret_n_p=n_p
+        else:
+            if d_n_p>0:
+                ret_n_p=current_n_p+0.5*delta_time
+            else:
+                ret_n_p=current_n_p-0.5*delta_time
+
+        current_angle=self.actuated_list['rudder_angle'][-1]
+        d_angle=rudder_angle-current_angle
+        if abs(d_angle)<0.5*delta_time:
+            ret_angle=rudder_angle
+        else:
+            if d_angle>0:
+                ret_angle=current_angle+0.5*delta_time
+            else:
+                ret_angle=current_angle-0.5*delta_time
+
+        self.actuated_list['n_p'].append(ret_n_p)
+        self.actuated_list['rudder_angle'].append(ret_angle)
+        actuated_msg.n_p=ret_n_p
+        actuated_msg.rudder_angle_degree=ret_angle
+        return actuated_msg
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node=ActuatorNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__=="__main__":
+    main()
---
+++ mmg_module/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+package_name = 'mmg_module'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='tabuchi',
+    maintainer_email='tabuchi@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+          "model_node = mmg_module.model_node:main",
+          "controller_node = mmg_module.controller_node:main",
+        ],
+    },
+)
