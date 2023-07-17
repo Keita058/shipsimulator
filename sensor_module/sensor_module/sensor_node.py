c41c6fb3bef3a9a748588d739268e95cf2f1a07d
Keita Tabuchi <tabuchi-keita-wm@ynu.jp>
2023-07-17 18:49:06 +0900

    complete sensor node

--- sensor_module/sensor_module/sensor_node.py
+++ sensor_module/sensor_module/sensor_node.py
@@ -5,13 +5,25 @@ from shipsim_msgs_module.msg import Control
 
 class SensorNode(Node):
     def __init__(self):
-        pass
+        super().__init__("sensor", namespace="ship1")
 
-    def listener_callback(self):
-        self.get_logger().info()
+        self.declare_parameter("subscribe_address1", "/ship1/obs_pose")
+        subscribe_address1=(self.get_parameter("subscribe_address1").get_parameter_value().string_value)
+        self.subscription = self.create_subscription(
+            Twist, subscribe_address1, self.listener_callback1, 10
+        )
 
-    def sender_callback(self):
-        pass
+        self.declare_parameter("subscribe_address2", "/ship1/obs_vel")
+        subscribe_address2=(self.get_parameter("subscribe_address2").get_parameter_value().string_value)
+        self.subscription = self.create_subscription(
+            Twist, subscribe_address2, self.listener_callback2, 10
+        )
+
+    def listener_callback1(self,msg):
+        self.get_logger().info('Subscribe: x="%ss", y="%s", psi="%s"'%(msg.linear.x, msg.linear.y, msg.angular.z))
+
+    def listener_callback2(self,msg):
+        self.get_logger().info('Subscribe: u="%ss", v="%s", r="%s"'%(msg.linear.x, msg.linear.y, msg.angular.z))
 
 
 def main(args=None):
