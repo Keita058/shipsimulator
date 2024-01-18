27c868d09773f03cb8f84cca70de321106103165
Keita Tabuchi <tabuchi-keita-wm@ynu.jp>
2024-01-18 13:47:53 +0900

    revise typo in disturbance setup.py

--- src/disturbance_module/setup.py
+++ src/disturbance_module/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'disturabnce_node = disturbance_module.disturbance_node:main',
+            'disturance_node = disturbance_module.disturbance_node:main',
         ],
     },
 )
