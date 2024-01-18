d6a05ca8d218471c68e768b303950af2ae18e5d6
Keita Tabuchi <tabuchi-keita-wm@ynu.jp>
2024-01-18 17:03:50 +0900

    change model node

--- src/mmg_module/mmg_module/model_node.py
+++ src/mmg_module/mmg_module/model_node.py
@@ -97,6 +97,7 @@ class ModelNode(Node):
     κ = self.get_parameter("κ").value
     tP = self.get_parameter("tP").value
     wpo = self.get_parameter("wpo").value
+    xP = self.get_parameter("xP").value
 
     # 操縦性微係数
     k0 = self.get_parameter("k0").value
@@ -126,55 +127,61 @@ class ModelNode(Node):
     mx = mx_*(0.5*ρ*L**2*d) # 付加質量x
     my = my_*(0.5*ρ*L**2*d) # 付加質量y
     Jzz = Jzz_*(0.5*ρ*L**4*d)
-    xG_ = xG/L # 無次元化
     xH = xH_*L
-    kx = 0.25*L # 慣動半径
     Izz = m*(0.25*L)**2 # 慣性モーメント[-]
     η = Dp/HR
     #U = np.sqrt(X[0]**2+(X[1]-X[2]*xG)**2) #合速度(重心周りで考える際にはこちら)
     #β = 0.0 if U==0.0 else np.arcsin(-(X[1]-X[2]*xG)/U) #斜航角(重心周りで考える際にはこちら)
-    U = np.sqrt(u_now**2+v_now**2) #合速度
-    β = 0.0 if U==0.0 else np.arcsin(-v_now/U) #斜航角
+    vm_now=v_now-xG*r_now #重心周りの横方向速度 
+    U = np.sqrt(u_now**2+vm_now**2) #合速度
+    β = 0.0 if U==0.0 else np.arctan(-vm_now/u_now) #斜航角
     
-    v_dash = 0.0 if U==0.0 else v_now/U #無次元化された横方向速度
+    vm_dash = 0.0 if U==0.0 else vm_now/U #無次元化された横方向速度
     r_dash = 0.0 if U==0.0 else r_now*L/U #無次元化された回頭角速度
-    J = 0.0 if n_p==0.0 else (1-wpo)*u_now/(n_p*Dp) #前進常数
+
+    β_P=β-xP*r_dash #舵の位置における斜航角
+    wP=wpo*np.exp(-4*β_P**2)
+    J = 0.0 if n_p==0.0 else (1-wP)*u_now/(n_p*Dp) #前進常数
     K_T = k0+k1*J+k2*J**2 #スラスト係数kx
-    v_R = U*γR*(β-lr_*r_dash) #舵に流入する横方向速度成分
+
+    β_R=β-lr_*r_dash
+    v_R = U*γR*β_R #舵に流入する横方向速度成分
     #v_R_ = γR*(β-lr_*r_dash) #無次元化
-    u_R = np.sqrt(η*(κ*ϵ*8.0*k0*n_p**2*Dp**4/np.pi)**2) if J==0.0 else u_now*(1-wpo)*ϵ*np.sqrt(η*(1.0+κ*(np.sqrt(1.0+8.0*K_T/(np.pi*J**2))-1))**2+(1-η)) #舵に流入する前後方向速度成分
+    u_R = np.sqrt(η*(κ*ϵ*8.0*k0*n_p**2*Dp**4/np.pi)**2) if J==0.0 else u_now*(1-wP)*ϵ*np.sqrt(η*(1.0+κ*(np.sqrt(1.0+8.0*K_T/(np.pi*J**2))-1))**2+(1-η)) #舵に流入する前後方向速度成分
     #u_R_ = (1-wpo)**2*ϵ**2*(η*(1.0+κ*(np.sqrt(1.0+8.0*K_T/(np.pi*J**2))-1))**2+(1-η)) #無次元化
     U_R = np.sqrt(u_R**2+v_R**2) #舵への流入速度
-    α_R = rudder_angle - np.arctan2(v_R,u_R) #舵への流入角度
+    α_R = rudder_angle - np.arctan(v_R/u_R) #舵への流入角度
     #α_R_ = X[6] - np.arctan2(v_R_,u_R_) #無次元化
     F_N = 0.5*AR*ρ*fa*(U_R**2)*np.sin(α_R)
     #F_N_ = AR/(L*d)*fa*(u_R_**2+v_R_**2)*np.sin(α_R_) #無次元化
 
     # 力の成分
-    X_H = 0.5*ρ*L*d*(U**2)*(-(R_0)+X_vv*v_dash**2+X_vr*v_dash*r_dash+X_rr*r_dash**2+X_vvvv*v_dash**4)
+    X_H = 0.5*ρ*L*d*(U**2)*(-(R_0)+X_vv*vm_dash**2+X_vr*vm_dash*r_dash+X_rr*r_dash**2+X_vvvv*vm_dash**4)
     #X_H = 0.5*ρ*L*d*(U**2)*(-R_0+X_vv*v_dash**2+(X_vr+m_+my_)*v_dash*r_dash+(X_rr+xG_*m_)*r_dash**2+X_vvvv*v_dash**4) #付加質量で調整
     X_R = -(1-tR)*F_N*np.sin(rudder_angle)
     X_P = (1-tP)*ρ*K_T*n_p**2*Dp**4
     
-    Y_H = 0.5*ρ*L*d*(U**2)*(Y_v*v_dash+Y_r*r_dash+Y_vvr*(v_dash**2)*r_dash+Y_vrr*v_dash*(r_dash**2)+Y_vvv*(v_dash**3)+Y_rrr*(r_dash**3))
+    Y_H = 0.5*ρ*L*d*(U**2)*(Y_v*vm_dash+Y_r*r_dash+Y_vvr*(vm_dash**2)*r_dash+Y_vrr*vm_dash*(r_dash**2)+Y_vvv*(vm_dash**3)+Y_rrr*(r_dash**3))
     #Y_H = 0.5*ρ*L*d*(U**2)*(Y_v*v_dash+(Y_r-m_-mx_)*r_dash+Y_vvr*(v_dash**2)*r_dash+Y_vrr*v_dash*(r_dash**2)+Y_vvv*(v_dash**3)+Y_rrr*(r_dash**3)) #付加質量で調整
     Y_R = -(1+aH)*F_N*np.cos(rudder_angle)
     
-    N_H = 0.5*ρ*(L**2)*d*(U**2)*(N_v*v_dash+N_r*r_dash+N_vvr*(v_dash**2)*r_dash+N_vrr*v_dash*(r_dash**2)+N_vvv*(v_dash**3)+N_rrr*(r_dash**3))
+    N_H = 0.5*ρ*(L**2)*d*(U**2)*(N_v*vm_dash+N_r*r_dash+N_vvr*(vm_dash**2)*r_dash+N_vrr*vm_dash*(r_dash**2)+N_vvv*(vm_dash**3)+N_rrr*(r_dash**3))
     #N_H = 0.5*ρ*(L**2)*d*(U**2)*(N_v*v_dash+(N_r-xG_*m_)*r_dash+N_vvr*(v_dash**2)*r_dash+N_vrr*v_dash*(r_dash**2)+N_vvv*(v_dash**3)+N_rrr*(r_dash**3)) #付加質量で調整
     N_R = -(-0.5+aH*xH)*F_N*np.cos(rudder_angle)
     
     # 状態計算
-    u_dot = ((X_H+X_R+X_P+self.X_D)+(m+my)*v_now*r_now+xG*m*r_now**2)/(m+mx)
+    u_dot = ((X_H+X_R+X_P+self.X_D)+(m+my)*vm_now*r_now+xG*m*r_now**2)/(m+mx)
     twist.linear.x = u_now + u_dot * delta_time
-
-    #d_v =(Y_H+Y_R-(m+mx)*X[0]*X[2]-xG*m*d_r)/(m+my)
-    v_dot = (xG**2*m**2*u_now*r_now-(N_H+N_R+self.N_d)*xG*m+((Y_H+Y_R+self.Y_d)-(m+mx)*u_now*r_now)*(Izz+Jzz+xG**2*m))/((Izz+Jzz+xG**2*m)*(m+my)-xG**2*m**2)
-    twist.linear.y = v_now + v_dot * delta_time
 
     #d_r = (N_H+N_R-xG*m*(d_v+X[0]*X[2]))/(Izz+Jzz+xG**2*m)
     r_dot = ((N_H+N_R+self.N_d)*(m+my)-xG*m*(Y_H+Y_R+self.Y_d-(m+mx)*u_now*r_now)-xG*m*u_now*r_now*(m+my))/((Izz+Jzz+xG**2*m)*(m+my)-xG**2*m**2)
+    r_next=r_now+r_dot*delta_time
     twist.angular.z = r_now + r_dot * delta_time
+
+    #d_v =(Y_H+Y_R-(m+mx)*X[0]*X[2]-xG*m*d_r)/(m+my)
+    vm_dot = (xG**2*m**2*u_now*r_now-(N_H+N_R+self.N_d)*xG*m+((Y_H+Y_R+self.Y_d)-(m+mx)*u_now*r_now)*(Izz+Jzz+xG**2*m))/((Izz+Jzz+xG**2*m)*(m+my)-xG**2*m**2)
+    vm_next=vm_now+vm_dot*delta_time
+    twist.linear.y = vm_next+xG*r_next
 
     return twist
 
@@ -204,6 +211,7 @@ class ModelNode(Node):
     self.declare_parameter("κ", 0.50)
     self.declare_parameter("tP", 0.220)
     self.declare_parameter("wpo", 0.40)
+    self.declare_parameter("xP", -0.650)
 
     # Maneuvering coefficients
     self.declare_parameter("k0", 0.2931)
